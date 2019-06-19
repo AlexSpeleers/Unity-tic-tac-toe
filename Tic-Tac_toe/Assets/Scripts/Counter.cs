e362f1f1088c8902c5bff0b72072f57ddff9d54a
AlexSpeleers <alexsieger@gmail.com>
2019-06-19 23:51:31 +0300

    push
    ffqwcq

---
+++ Tic-Tac_toe/Assets/Scripts/Counter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Counter : MonoBehaviour
+{
+    public int gridNumber = 0;
+        
+    private void OnMouseDown() => GameObject.Find("Main Camera").GetComponent<PlayerController>().SendMessage("GridClicked", gameObject);
+    
+}
---
+++ Tic-Tac_toe/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour
+{
+   // [SerializeField]
+    //List<GameObject> grid = new List<GameObject>();
+    public float rayLenght;
+    public LayerMask layerMask;
+    public GameObject cross;
+    public GameObject ring;
+    int turn = 1;
+    int[] grids = new int[9];
+
+    void NextTurn()
+    {
+        turn += 1;
+        if (turn == 3)
+            turn = 1;
+    }
+
+    public void GridClicked(GameObject grid)
+    {
+        int gridNumber = grid.GetComponent<Counter>().gridNumber;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonUp(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            RaycastHit hit = new RaycastHit();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            //переменная для ханения номера клетки
+            int gridNumber = hit.transform.GetComponent<Counter>().gridNumber;
+            
+            //присваиваем номеру клетки номер игрока
+            grids[gridNumber] = turn;
+
+            if (Physics.Raycast(ray, out hit, rayLenght, layerMask))
+            {
+                if (turn == 1)
+                    Instantiate(cross, hit.transform.position, Quaternion.identity);
+                
+                else
+                    Instantiate(ring, hit.transform.position, Quaternion.Euler(90, 0, 0));
+
+                NextTurn();
+                Destroy(hit.transform.gameObject);
+            }
+        }
+    }
+}
